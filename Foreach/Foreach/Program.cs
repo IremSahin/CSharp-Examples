6690d026dcfd83b36d50c552977bd326db1e0ac4
IremSahin <38790270+IremSahin@users.noreply.github.com>
2018-04-28 20:57:39 +0300

    Foreach
    An example of a simple foreach loop.

---
+++ Foreach/Foreach/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] sayi = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                sayi[i] = rnd.Next(10,50);
+            }
+
+            foreach (var item in sayi)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
