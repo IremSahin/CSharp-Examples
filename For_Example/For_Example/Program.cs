a295d4d5d658ac272f6dafc4e6b767b339f7b8d5
IremSahin <38790270+IremSahin@users.noreply.github.com>
2018-04-29 16:44:50 +0300

    For_Example
    It is a simple example of a for loop.

---
+++ For_Example/For_Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace For_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1-100 aralığındaki çift sayıların toplamını ve ortalaması
+            decimal ortalama = 0M;
+            decimal toplam = 0M;
+            int sayac = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i%2 == 0)
+                {
+                    toplam += i;
+                    sayac++;
+                }
+            }
+            ortalama = toplam / sayac;
+            Console.Write("Toplam: " + toplam);
+            Console.Write("\nOrtalama: " + ortalama);
+            Console.ReadKey();
+        }
+    }
+}
